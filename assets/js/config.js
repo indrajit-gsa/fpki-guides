036a9b78d5f00d7d2c44ade3059b3e128cfd991c
Jordan Packham <jordan.packham@gsa.gov>
2018-03-28 19:16:02 -0400

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 /*** USE THIS FILE TO SET OPTIONS ***/
 
 setParams({
-    graphFile : "/graph/fpki-certs.gexf",
+    graphFile : "../../graph/fpki-certs.gexf",
         /*
             The GEXF file to show ! -- can be overriden by adding
             a hash to the document location, e.g. index.html#celegans.gexf
